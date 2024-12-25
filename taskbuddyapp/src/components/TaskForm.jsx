a75a7eadc7009b537eb2af72848b604412da56a0
rohithsaikumar09 <grohithsaikumar29@gmail.com>
2024-12-25 20:22:56 +0530

    Final Code

--- taskbuddyapp/src/components/TaskForm.jsx
+++ taskbuddyapp/src/components/TaskForm.jsx
@@ -1,34 +1,44 @@
 import React, { useState } from 'react'
 
-export default function TaskForm() {
+export default function TaskForm({addTask}) {
     const [task, setTask] = useState('');
-    const [priority, setPriority] = useState('Medium');
-    const [category, setCategory] = useState('General');
-  return (
-    <form className='task-form'>
-      <div id='inp'>
-      <input type='text'
-        placeholder='Enter Your Task'
-        required
-        value = {task}
-        onChange={(e)=> setTask(e.target.value)}/>
+    const [priority, setPriority] = useState("Medium");
+    const [category, setCategory] = useState("General");
+
+    const handlesubmit =(e)=>{
+        e.preventDefault();
+        addTask({text: task, priority, category,completed: false}); //send data to addTask()
         
-        <spam><button>Add Task</button></spam>
+        //Reset state
+        setPriority("Medium");
+        setCategory('General');
+        setTask("");
+    }
+    
+  return (
+    <form onSubmit={handlesubmit} className='task-form'>
+    <div id="inp">
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter your task"
+        required
+      /> <span><button type="submit">Add Task</button></span>
+    </div>
+
+    <div id="btns">
+      <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="General">General</option>
+        <option value="Work">Work</option>
+        <option value="Personal">Personal</option>
+      </select>
       </div>
-        <div className='btns'>
-            <select value={priority} onChange={(e)=> setPriority(e.target.value)}>
-                <option>Medium</option>
-                <option>Low</option>
-                <option>High</option>
-            </select>
-
-            <select value={category}onChange={(e)=> setCategory(e.target.value)}>
-                <option>General</option>
-                <option>Work</option>
-                <option>Personal</option>
-            </select>
-        </div>
-
     </form>
   )
 }
